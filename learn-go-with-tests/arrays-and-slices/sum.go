1900273155a73ffa57ae948214070180fcba508b
frnsimoes <simoes.grcia@gmail.com>
2021-05-30 19:17:29 -0300

    finished arrays and slices

--- learn-go-with-tests/arrays-and-slices/sum.go
+++ learn-go-with-tests/arrays-and-slices/sum.go
@@ -1,6 +1,6 @@
 package main
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
@@ -15,4 +15,27 @@ func SumSlice(numbers []int) int {
 	}
 
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+	return sums
+
+}
+
+func main() {
+
 }
