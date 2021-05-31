5b2b1bb76b6b3f266644951d18aabb319a18c4d1
frnsimoes <simoes.grcia@gmail.com>
2021-05-30 22:21:40 -0300

    started structs

---
+++ learn-go-with-tests/structs-methods-interfaces/main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+
+	t.Run("testing a rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := Perimeter(rectangle)
+		want := 40.0
+
+		if got != want {
+			t.Errorf("got %.2f, want %.2f", got, want)
+		}
+	})
+
+	t.Run("testing a circle", func(t *testing.T) {
+		circle := Circle{10}
+		got := Area(circle)
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+}
+
+func TestArea(t *testing.T) {
+	rectangle := Rectangle{12.0, 6.0}
+
+	got := Area(rectangle)
+	want := 72.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+
+	}
+}
